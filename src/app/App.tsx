72310890072958959da471f26af2203d220e9a6e
serviciosapcsoporte-create <serviciosapcsoporte@gmail.com>
2026-07-19 12:19:43 -0500

    Cristal a la izquierda: subir social-float para no solaparse

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1347,8 +1347,8 @@ export default function App() {
         </div>
       </footer>
 
-      {/* Social float (left, does not overlap WhatsApp float) */}
-      <div className="fixed bottom-6 left-6 z-50 flex flex-col gap-3">
+      {/* Social float (left, above Cristal bot) */}
+      <div className="fixed bottom-24 left-6 z-50 flex flex-col gap-3">
         <a
           href="https://www.facebook.com/alejandropiraquiveapc"
           target="_blank"
